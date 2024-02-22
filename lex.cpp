e4454533eef9378a86003f772eae3b38023dafe9
Justin <59674603+Toash@users.noreply.github.com>
2024-02-22 11:07:11 -0500

    starting comment recognition on lexical analyzer

--- lex.cpp
+++ lex.cpp
@@ -7,11 +7,43 @@ LexItem getNextToken(istream &in, int &linenumber)
     enum State
     {
         START,
-        INID
-    } char c;
+        INID,
+        INSTRING,
+        ININT,
+        INCOMMENT
+    };
+    State state = START;
+    char c;
     while (in.get(c))
     {
-        // Recognize the lexemes (with a corresponding token)
-        //      To produce a LexItem.
+        switch (state)
+        {
+        case START:
+            // new lines are delimited by !
+            if (c = '\n')
+            {
+                linenumber++;
+            }
+
+            if (std::isspace(c))
+            {
+                continue;
+            }
+
+            // Comment is defined by all characters after !
+            if (c == '!')
+            {
+                state = INCOMMENT;
+                continue
+            }
+        }
+    case INCOMMENT:
+        // comment lasts till end of line. does not have token.
+        if (c == '\n')
+        {
+            linenumber++;
+            state = START;
+        }
+        continue;
     }
 }
---
+++ test.cpp
@@ -0,0 +1,63 @@
+// Test the lexical analyzer
+#include <iostream>
+#include <fstream> // input file
+#include <string>
+#include "lex.h"
+
+/*
+● -all (optional): if present, every token is printed out when it is seen followed by its lexeme
+using the format described below.
+● -int (optional): if present, prints out all the unique integer constants in numeric order.
+● -real (optional): if present, prints out all the unique real constants in numeric order.
+● -str (optional): if present, prints out all the unique string constants in order
+● -id (optional): if present, prints out all of the unique identifiers in alphanumeric order. Each
+identifier is followed by the number of its occurrences between parentheses.
+● -kw (optional): if present, prints out all of the unique keywords in alphabetical order. Each
+keyword is followed by the number of its occurrences between parentheses.
+● filename argument must be passed to main function. Your program should open the file
+and read from that filename.
+*/
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        std::cout << "NO SPECIFIED INPUT FILE.\n";
+        exit(1);
+    }
+
+    std::string fileName = argv[1];
+    std::ifstream inFile;
+    inFile.open(fileName);
+    if (inFile.fail())
+    {
+        std::cout << "CANNOT OPEN THE FILE: ";
+        std::cout << fileName << std::endl;
+        exit(1);
+    }
+
+    inFile.seekg(0, std::ios::end);
+    if (inFile.tellg() == 0)
+    {
+        std::cout << "File is empty.\n";
+        exit(1);
+    }
+
+    // Repeatedly call getNextToken until it returns DONE or ERR.
+    LexItem token;
+    int linenumber = 0;
+    if (inFile.is_open())
+    {
+        while (true)
+        {
+            token = getNextToken(inFile, linenumber);
+
+            // Equals operator is overridden
+            if (token == DONE)
+            {
+                break;
+            }
+        }
+    }
+    return 0;
+}
